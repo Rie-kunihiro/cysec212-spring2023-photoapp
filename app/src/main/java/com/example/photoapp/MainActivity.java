add561e0ea06b64058d16b28d733fd5aa66cc364
Rie <hasegawa001@gannon.edu>
2023-03-21 19:00:17 -0400

    Added Intent for take picture

--- app/src/main/java/com/example/photoapp/MainActivity.java
+++ app/src/main/java/com/example/photoapp/MainActivity.java
@@ -2,9 +2,20 @@ package com.example.photoapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
 
 public class MainActivity extends AppCompatActivity {
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    private void dispatchTakePictureIntent(){
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null){
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
